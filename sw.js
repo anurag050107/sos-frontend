b8ca61cb011ffa40e0127fcc4112c95b9111ff74
Anurag Rai <aadamgamer07@gmail.com>
2026-05-23 12:54:42 +0530

    sos not working function changed

--- sw.js
+++ sw.js
@@ -1,36 +1,14 @@
-const CACHE_NAME = "campus-sos-v10-final";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/admin.html"
+const CACHE_NAME = 'resilient-campus-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
-self.addEventListener("install", event => {
-    self.skipWaiting(); // Force the new service worker to activate immediately
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", event => {
-    // Delete all old caches so the browser is forced to download the new HTML
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
